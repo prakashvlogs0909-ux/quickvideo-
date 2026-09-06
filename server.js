6d28eebe39c6079bf03e235a41f0826949abe1df
prakashvlogs0909-ux <prakashvlogs0909@gmail.com>
2026-09-06 07:53:49 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "online",
+    message: "QuickVideo backend is running!"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
